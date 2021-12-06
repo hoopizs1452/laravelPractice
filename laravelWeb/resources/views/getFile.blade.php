e8dcf49c3b7360f63183f0f9f6da22f831a13287
hoopizs1452 <hoopizs1452@gmail.com>
2021-12-06 18:17:11 +0800

    update code

--- laravelWeb/resources/views/getFile.blade.php
+++ laravelWeb/resources/views/getFile.blade.php
@@ -5,12 +5,21 @@
 
 <div>
     <?php
+        //::的第一種用法，Controller的function需加static
         // use Illuminate\Support\Facades\Route;
         // use App\Http\Controllers\UploadController;
 
         // echo UploadController::getAllFile();
 
-        echo App\Http\Controllers\UploadController::getAllFile();
+        //::的第二種用法，Controller的function需加static
+        //echo App\Http\Controllers\UploadController::getAllFile();
+
+        //Controller不用static
+        use Illuminate\Support\Facades\Route;
+        use App\Http\Controllers\UploadController;
+
+        $file = new UploadController();
+        echo $file->getAllFile();
         
     ?>
 </div>
